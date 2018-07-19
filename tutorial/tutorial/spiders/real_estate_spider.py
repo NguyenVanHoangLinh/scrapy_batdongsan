d8eb0e53fcdae4c13deacef8bb59fb9be40696c1
NguyenVanHoangLinh <nguyenvan.hoanglinh@yahoo.com>
2018-07-19 10:10:06 +0700

    fix date comparision

--- tutorial/tutorial/spiders/real_estate_spider.py
+++ tutorial/tutorial/spiders/real_estate_spider.py
@@ -50,4 +50,5 @@ class RealEstateSpider(scrapy.Spider):
         start_date =dt.strptime(estate_date,"%d-%m-%Y")
         end_date =dt.strptime(estate_expired_date,"%d-%m-%Y")
         estateItem = real_estateItem(estate_title= estate_title,estate_address=estate_address,estate_area=estate_area,estate_description=estate_description,estate_price=estate_price,estate_type=estate_type,estate_tag=estate_tag,estate_date=estate_date,estate_seller_name=estate_seller_name,estate_seller_address=estate_seller_address,estate_seller_phone=estate_seller_phone,estate_seller_mobile=estate_seller_mobile,estate_seller_email=estate_seller_email)
-        yield estateItem
+        if start_date <= end_date:
+            yield estateItem
