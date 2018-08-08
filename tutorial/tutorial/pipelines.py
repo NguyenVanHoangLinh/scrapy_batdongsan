d36f16b8b313fce9758f93c5bc099559b2006aa9
NguyenVanHoangLinh <nguyenvan.hoanglinh@yahoo.com>
2018-08-08 15:39:27 +0900

    connection.cursor()

--- tutorial/tutorial/pipelines.py
+++ tutorial/tutorial/pipelines.py
@@ -16,11 +16,11 @@ class TutorialPipeline(object):
         self.cur = self.connection.cursor()
 
     def close_spider(self, spider):
-        self.cur.close()
+        self.connection.cursor().close()
         self.connection.close()
 
     def process_item(self, item, spider):
-        self.cur.execute("insert into estate(estate_title,estate_address,estate_area,estate_description,estate_price,estate_type,estate_tag,estate_date,estate_seller_name,estate_seller_address,estate_seller_phone,estate_seller_mobile,estate_seller_email) values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",(item['estate_title'],item['estate_address'],item['estate_area'],item['estate_description'],item['estate_price'],item['estate_type'],item['estate_tag'],item['estate_date'],item['estate_seller_name'],item['estate_seller_address'],item['estate_seller_phone'],item['estate_seller_mobile'],item['estate_seller_email']))
+        self.connection.cursor().execute("insert into estate(estate_title,estate_address,estate_area,estate_description,estate_price,estate_type,estate_tag,estate_date,estate_seller_name,estate_seller_address,estate_seller_phone,estate_seller_mobile,estate_seller_email) values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",(item['estate_title'],item['estate_address'],item['estate_area'],item['estate_description'],item['estate_price'],item['estate_type'],item['estate_tag'],item['estate_date'],item['estate_seller_name'],item['estate_seller_address'],item['estate_seller_phone'],item['estate_seller_mobile'],item['estate_seller_email']))
         self.connection.commit()
         return item
     
